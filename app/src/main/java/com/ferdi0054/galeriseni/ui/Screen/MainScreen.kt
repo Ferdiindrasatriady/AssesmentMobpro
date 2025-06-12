55f1a45c31613d60d59d132f2d79e9860b74e2b8
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-06-12 22:13:09 +0700

    menambahkan ikon profil(ada yg error)

--- app/src/main/java/com/ferdi0054/galeriseni/ui/Screen/MainScreen.kt
+++ app/src/main/java/com/ferdi0054/galeriseni/ui/Screen/MainScreen.kt
@@ -1,8 +1,13 @@
 package com.ferdi0054.galeriseni.ui.Screen
 
+import android.content.Context
 import android.content.res.Configuration
+import android.credentials.CredentialManager
+import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,10 +15,21 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.viewbinding.BuildConfig
 import com.ferdi0054.galeriseni.R
+import com.ferdi0054.galeriseni.model.User
+import com.ferdi0054.galeriseni.network.UserDataStore
 import com.ferdi0054.galeriseni.ui.theme.GaleriSeniTheme
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,7 +43,17 @@ fun MainScreen (){
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
-                )
+                ),
+                actions = {
+                    IconButton(onClick = {
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_account_circle_24),
+                            contentDescription = stringResource(id = R.string.profile),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
@@ -40,6 +66,43 @@ fun ScrenContent(modifier: Modifier = Modifier){
         text = "Hello Android",
         modifier = modifier
     )
+}
+private suspend fun signIn(context: Context,dataStore: UserDataStore){
+val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+    .setFilterByAuthorizedAccounts(false)
+    .setServerClientId(BuildConfig.API_KEY)
+    .build()
+    val request: GetCredentialRequest = GetCredentialRequest.Builder()
+        .addCredentialOption(googleIdOption)
+        .build()
+    try {
+        val credentialManager = CredentialManager.create(context)
+        val result = credentialManager.getCredential(context,request)
+        handleSignIn(result,dataStore)
+    } catch (e: GetCredentialException){
+        Log.e("Sign-In", "Error: ${e.errorMessage}")
+    }
+}
+private suspend fun handleSignIn(
+    result: Result:GetCredentialResponse,
+    dataStore: UserDataStore
+) {
+    val credential = result.credential
+    if (credential is CustomCredential&&
+        credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+        ) {
+        try {
+            val googleId = GoogleIdTokenCredential.createFrom(credential.data)
+            val nama = googleId.displayName?:""
+            val email = googleId.id
+            val photoUrl = googleId.profilePictureUri.toString()
+            dataStore.saveData(User(nama, email, photoUrl))
+        } catch (e: GoogleIdTokenParsingException){
+            Log.e("SIGN-IN", "Error: ${e.message}")
+        }
+    } else{
+        Log.e("SIGN-IN", "Error: unrecpgmozed custom credential type.")
+    }
 }
 
 @Preview(showBackground = true)
