cc6444d6350cc04d00d0dafb87c7729ee2abf611
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-06-13 14:50:06 +0700

    membuat foto jadi bulat dan button di kiri kanan

--- app/src/main/java/com/ferdi0054/galeriseni/ui/Screen/ProfileDialog.kt
+++ app/src/main/java/com/ferdi0054/galeriseni/ui/Screen/ProfileDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -55,7 +57,9 @@ fun ProfilDialog(
                     contentScale = ContentScale.Crop,
                     placeholder = painterResource(id = R.drawable.loading_img),
                     error = painterResource(id = R.drawable.baseline_broken_image_24),
-                    modifier = Modifier.size(100.dp)
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
                 )
                 Text(
                     text = user.name,
@@ -81,16 +85,17 @@ fun ProfilDialog(
                     ) {
                         Text(stringResource(R.string.tutup))
                     }
-                }
-                OutlinedButton(
-                    onClick = { onConfirmation() },
-                    modifier = Modifier.padding(8.dp),
-                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.error)
-                ) {
-                    Text(
-                        text = stringResource(R.string.logout),
-                        color = MaterialTheme.colorScheme.error
-                    )
+                    OutlinedButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.error)
+                    ) {
+
+                        Text(
+                            text = stringResource(R.string.logout),
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
                 }
             }
         }
