3dff4141ac660b64be9e7647d20e7ec5f8ce760d
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-06-13 23:50:29 +0700

    optimize

--- app/src/main/java/com/ferdi0054/galeriseni/ui/screen/MainScreen.kt
+++ app/src/main/java/com/ferdi0054/galeriseni/ui/screen/MainScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
